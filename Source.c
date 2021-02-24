475723182e708fe072ded9cf0c1fff176cd6d8bb
shadrinanzhela <71284074+shadrinanzhela@users.noreply.github.com>
2021-02-24 23:34:48 +0700

    Update Source.c
    new comments

--- Source.c
+++ Source.c
@@ -4,47 +4,47 @@
 #include <math.h>
 #include <time.h>
 
- 
+
 
 int compare_even(int* a, int* b)//for even numbers
 {
 	return(*a - *b);
 }
-	
+
 int compare_odd(int* a, int* b) //for odd numbers
 {
 	return(*b - *a);
 }
 
-int** func_random_arrays(int n) //функция возвращает массив указателей на отсортированные массивы, в первой ячейке которых хранится длины
+int** func_random_arrays(int n) //return array of arrays where arr[0]=size of arr 
 {
 	srand(time(NULL));
 
-	if (n >= RAND_MAX) //не найдется n различных рандомных чисел, если n будет больше, чем максимально возможное рандомное число
+	if (n >= RAND_MAX) // max count of different size arrays can't be more than RAND_MAX
 	{
 		return(NULL);
 	}
-	
-	int** arr = (int**)malloc(sizeof(int*) * n);    
-	int* ver = (int*)malloc(sizeof(int) * (RAND_MAX + 2)); //массив в котором будет храниться информация о уже использованных размерах массивов
+
+	int** arr = (int**)malloc(sizeof(int*) * n);
+	int* ver = (int*)malloc(sizeof(int) * (RAND_MAX + 2)); //used sizes array
 	for (int i = 0; i < (RAND_MAX + 2); i++)
 		ver[i] = 0;
 
 	int size;
 	for (int i = 0; i < n; i++)
 	{
-		size = rand() + 1; // генерация новой длины
-		while (ver[size]) 
-			size = rand() + 1; 
-		ver[size] = 1; 
+		size = rand() + 1; // new size generation
+		while (ver[size])
+			size = rand() + 1;
+		ver[size] = 1;
 
-		arr[i] = (int*)malloc(sizeof(int) * (size + 1)); //выделение и заполнение массива
-		arr[i][0] = size; //храним в arr[i][0] длинну массива, сам массив заполняется и сортируется с arr[i][1] элемента
-		int* arr_el = arr[i] + 1; 
-		for (int j = 0; j < size; j++) 
+		arr[i] = (int*)malloc(sizeof(int) * (size + 1)); //new array generation 
+		arr[i][0] = size; // arr[i][0] contains size, array content start from arr[i][1]
+		int* arr_el = arr[i] + 1;
+		for (int j = 0; j < size; j++)
 			arr_el[j] = rand();
 
-		if (i & 1)//  сортировка в зависимотсти от четности
+		if (i & 1)
 			qsort(arr_el, size, sizeof(int), compare_odd);
 		else
 			qsort(arr_el, size, sizeof(int), compare_even);
@@ -63,37 +63,39 @@ int main()
 	int n;
 	scanf("%d", &n);
 	int** arr = func_random_arrays(n);
-	if (arr == NULL) //проверка на слишком большое n
+	if (arr == NULL)
 		while (!arr)
 		{
-			printf("Value should not exceed, if you want exit program enter '-1'\n", RAND_MAX);
+			printf("Value should not exceed %d, if you want exit program enter '-1'\n", RAND_MAX);
 			scanf("%d", &n);
 			if (n < 0)
 				return(0);
 			arr = func_random_arrays(n);
 		}
-	
-	for (int i = 0; i < n; i++)//вывод массивов в output.txt
+
+	for (int i = 0; i < n; i++)//print arrays to output.txt
 	{
+		fprintf(fw, "size = %d array:\n", arr[i][0]);
 		for (int j = 1; j < arr[i][0]; j++)
 			fprintf(fw, "%d ", arr[i][j]);
 		fprintf(fw, "\n");
 	}
-	
-	//запрос на вывод и вывод массивов в консоль 
-	printf("Look at the arrays in the file 'output.txt' please, if you want to output them to the console enter 'yes' else 'no'\n");
+
+
+	printf("Look at the arrays in the file 'output.txt' please, if you want print them to the console enter 'yes' else 'no'\n");
 	char str[4];
-	scanf("%s", str); 
+	scanf("%s", str);
 	int flag = 1;
 	while (flag)
 	{
-		if ((str[0] == 'y') || (str[0] == 'Y')) 
+		if ((str[0] == 'y') || (str[0] == 'Y'))
 		{
 			for (int i = 0; i < n; i++)
 			{
+				printf("size = %d array:\n", arr[i][0]);
 				for (int j = 1; j < arr[i][0]; j++)
 					printf("%d ", arr[i][j]);
-				printf("\n");
+				printf("\n\n");
 			}
 			flag = 0;
 		}
